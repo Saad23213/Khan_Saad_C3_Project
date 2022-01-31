8c701c452b3d7c371053383ef4e2664566f26f91
Saad23213 <khansaadyt@gmail.com>
2022-01-31 23:13:26 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -7,18 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RestaurantTest {
     Restaurant restaurant;
+
     //REFACTOR ALL THE REPEATED LINES OF CODE
-    public void restTest(){
+    public void restTest() {
         LocalTime openingTime = LocalTime.parse("10:30:00");
         LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant = new Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
+        restaurant.addToMenu("Sweet corn soup", 119);
         restaurant.addToMenu("Vegetable lasagne", 269);
     }
+
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
     @Test
-    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time() {
         restTest();
         restaurant.openingTime = LocalTime.now().minusMinutes(10);
         restaurant.closingTime = LocalTime.now().plusMinutes(10);
@@ -27,7 +29,7 @@ class RestaurantTest {
     }
 
     @Test
-    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time() {
         restTest();
         restaurant.openingTime = LocalTime.now().plusMinutes(10);
         restaurant.closingTime = LocalTime.now().minusMinutes(10);
@@ -40,28 +42,36 @@ class RestaurantTest {
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @Test
-    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+    public void adding_item_to_menu_should_increase_menu_size_by_1() {
 
         restTest();
         int initialMenuSize = restaurant.getMenu().size();
-        restaurant.addToMenu("Sizzling brownie",319);
-        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+        restaurant.addToMenu("Sizzling brownie", 319);
+        assertEquals(initialMenuSize + 1, restaurant.getMenu().size());
     }
+
     @Test
     public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
 
         restTest();
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.removeFromMenu("Vegetable lasagne");
-        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+        assertEquals(initialMenuSize - 1, restaurant.getMenu().size());
     }
+
     @Test
     public void removing_item_that_does_not_exist_should_throw_exception() {
 
         restTest();
         assertThrows(itemNotFoundException.class,
-                ()->restaurant.removeFromMenu("French fries"));
+                () -> restaurant.removeFromMenu("French fries"));
     }
 
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    @Test
+    public void selected_items_from_menu_should_return_total_order_cost() throws itemNotFoundException {
+        restTest();
+        int totalCost=0;
+        assertEquals(400, totalCost);
+    }
 }
